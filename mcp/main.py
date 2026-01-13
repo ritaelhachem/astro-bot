ec3acc5247e2e80221b920613ee909ef6f53958d
rbs-christian <christian.randrianaivoson@epitech.digital>
2026-01-13 16:52:05 +0100

    Ajout de tools et suppressions des tools non pertinents

--- mcp/main.py
+++ mcp/main.py
@@ -2,10 +2,6 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 
 # Import des tools
-from tools.scraping import scrape_latest_news
-from tools.calculate import calculate
-from tools.summarize import summarize
-from tools.search import search_keyword
 from tools.scrape_rss import scrape_astronomy_news
 from tools.astronomy_image_search import astronomy_image_search
 from tools.celestial_position import celestial_position
@@ -18,19 +14,6 @@ app = FastAPI(title="MCP Server")
 # -----------------------------
 # MODELES DE REQUÊTES
 # -----------------------------
-
-class ScrapeRequest(BaseModel):
-    url: str
-
-class CalculateRequest(BaseModel):
-    expression: str
-
-class SummarizeRequest(BaseModel):
-    text: str
-
-class SearchRequest(BaseModel):
-    text: str
-    keyword: str
 
 class AstronomyNewsRequest(BaseModel):
     keyword: str | None = None
@@ -49,42 +32,6 @@ class CelestialPositionRequest(BaseModel):
 # -----------------------------
 # ROUTES DES TOOLS
 # -----------------------------
-
-@app.post("/tools/scrape")
-def scrape_tool(request: ScrapeRequest):
-    data = scrape_latest_news(request.url)
-    return {
-        "tool": "scrape",
-        "input": request.url,
-        "output": data
-    }
-
-@app.post("/tools/calculate")
-def calculate_tool(request: CalculateRequest):
-    data = calculate(request.expression)
-    return {
-        "tool": "calculate",
-        "input": request.expression,
-        "output": data
-    }
-
-@app.post("/tools/summarize")
-def summarize_tool(request: SummarizeRequest):
-    data = summarize(request.text)
-    return {
-        "tool": "summarize",
-        "input": request.text,
-        "output": data
-    }
-
-@app.post("/tools/search")
-def search_tool(request: SearchRequest):
-    data = search_keyword(request.text, request.keyword)
-    return {
-        "tool": "search",
-        "input": {"text": request.text, "keyword": request.keyword},
-        "output": data
-    }
 
 @app.post("/tools/scrape_astronomy_news")
 def scrape_astronomy_news_tool(request: AstronomyNewsRequest):
